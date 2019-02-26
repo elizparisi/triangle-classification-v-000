81a761c16165924991ab38557bf3e1a88bb428b2
Elizabeth Parisi <parisielizabeth@gmail.com>
2019-02-26 02:30:27 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,50 @@
 class Triangle
   # write code here
+  
+  attr_accessor :side_a, :side_b, :side_c
+  
+  @triangle_sides = []
+  
+  def intialize(side_a, side_b, side_c)
+    @triangle_sides << side_a
+    @triangle_sides << side_b
+    @triangle_sides << side_c
+  end
+  
+    def valid?
+    sum_one_two = @triangle_sides[0] + @triangle_sides[1]
+    sum_one_three = @triangle_sides[0] + @triangle_sides[2]
+    sum_two_three = @triangle_sides[1] + @triangle_sides[2]
+
+    if (@triangle_sides.none? {|side| side <= 0}) &&
+      (sum_one_two > @triangle_sides[2] && sum_one_three > @triangle_sides[1] && sum_two_three > @triangle_sides[0])
+      return true
+    else
+      return false
+    end
+  end
+
+  def kind
+    if valid?
+      if @triangle_sides.uniq.length == 1
+        return :equilateral
+      elsif @triangle_sides.uniq.length == 2
+        return :isosceles
+      else
+        return :scalene
+      end
+    else
+      raise TriangleError
+    end
+  end
 end
+ 
+  class TriangleError < StandardError
+    # triangle error code
+    def message
+      "This is not a valid triangle!"
+    end
+  end
+end
+
+
